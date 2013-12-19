cdcb03ec8a6542acfdac9b3fcf06aad7927af4ba
Alexandre Sampaio <allexonrails@gmail.com>
2013-12-19 08:57:07 -0200

    Inicio da Rodada

---
+++ controle.estoque_ti/src/br/smagp/controle/estoque_ti/run_framei.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.smagp.controle.estoque_ti;
+
+/**
+ *
+ * @author AllexOnRails
+ */
+public class run_framei {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
